8c23de70a39c88192c8548dd0b02aaa8c46fb89d
MatValdejao <matvaldejao123@gmail.com>
2022-10-06 00:44:31 -0400

    fit schema name

--- models/Thought.js
+++ models/Thought.js
@@ -1,66 +1,66 @@
-const { Schema, model, Types } = require("mongoose")
-const dateFormat = require("../utils/dateFormat")
+const { Schema, model, Types } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
 
-const ReplySchema = new Schema(
-    {
-        reactionId: {
-            type: Schema.Types.ObjectId,
-            default: () => new Types.ObjectId(),
-        },
-        reactionBody: {
-            type: String,
-            require: true,
-            maxLength: 280,
-        },
-        username: {
-            type: String,
-            require: true,
-        }, 
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            get: (createdAtVal) => dateFormat(createdAtVal),
-        }
-    },
-    {
-        toJSON: {
-            getters: true,
-        }
-    }
-)
+const reactionSchema = new Schema(
+	{
+		reactionId: {
+			type: Schema.Types.ObjectId,
+			default: () => new Types.ObjectId(),
+		},
+		reactionBody: {
+			type: String,
+			require: true,
+			maxLength: 280,
+		},
+		username: {
+			type: String,
+			require: true,
+		},
+		createdAt: {
+			type: Date,
+			default: Date.now,
+			get: (createdAtVal) => dateFormat(createdAtVal),
+		},
+	},
+	{
+		toJSON: {
+			getters: true,
+		},
+	}
+);
 
 const ThoughtSchema = new Schema(
-    {
-        thoughtText: {
-            type: String,
-            require: true,
-            maxLength: 280,
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            get: (createdAtVal) => dateFormat(createdAtVal),
-        },
-        username: {
-            type: String, 
-            require: true,
-        },
-        reactions: [reactionSchema],
-    },
-    {
-        toJSON: {
-            virtuals: true,
-            getters: true,
-        },
-        id: false,
-    }
-)
+	{
+		thoughtText: {
+			type: String,
+			require: true,
+			maxLength: 280,
+		},
+		createdAt: {
+			type: Date,
+			default: Date.now,
+			get: (createdAtVal) => dateFormat(createdAtVal),
+		},
+		username: {
+			type: String,
+			require: true,
+		},
+		reactions: [reactionSchema],
+	},
+	{
+		toJSON: {
+			virtuals: true,
+			getters: true,
+		},
+		id: false,
+	}
+);
 
 ThoughtSchema.virtual("reactionCount").get(function () {
-    return this.reactions.length
-})
+	return this.reactions.length;
+});
 
 // create model
-const Thought = model("Thought", ThoughtSchema)
+const Thought = model("Thought", ThoughtSchema);
 
-module.exports = Thought
+module.exports = Thought;
